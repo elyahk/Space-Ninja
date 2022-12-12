afc529236333f01b0384830da0d5193ff39526f4
elyahk-apple-academy <enusratov22@fed.idserve.net>
2022-12-12 15:56:34 +0100

    Change to SwiftUI project

---
+++ Space Ninja/Space_NinjaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Space_NinjaApp.swift
+//  Space Ninja
+//
+//  Created by Eldorbek Nusratov on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Space_NinjaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
