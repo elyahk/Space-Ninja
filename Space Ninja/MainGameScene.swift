17fd6781e475dcc4ea7fffa4329bf0680b40ba68
elyahk-apple-academy <enusratov22@fed.idserve.net>
2022-12-13 14:18:37 +0100

    Make Lyon running

--- Space Ninja/MainGameScene.swift
+++ Space Ninja/MainGameScene.swift
@@ -12,6 +12,32 @@ let height = UIScreen.main.bounds.width
 let width = UIScreen.main.bounds.height
 
 // A simple game scene with falling boxes
+
+enum KYOAction: String {
+    case run = "kyo_run"
+    case jump = "kyo_jump"
+    
+    var actionNames: [String] {
+        var actions: [String] = ["kyo_run"]
+        
+        return actions
+    }
+}
+
+class KYOPriteNode: SKSpriteNode {
+    func makeAction(type action: KYOAction) {
+        let actionTextures =
+        [
+            SKTexture(imageNamed: "kyo_run_01"),
+            SKTexture(imageNamed: "kyo_run_02"),
+            SKTexture(imageNamed: "kyo_run_03")
+        ]
+        
+        let action = SKAction.animate(with: actionTextures, timePerFrame: 0.1, resize: true, restore: true)
+        run(SKAction.repeatForever(action))
+    }
+}
+
 class GameScene: SKScene {
     override func didMove(to view: SKView) {
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
@@ -20,28 +46,16 @@ class GameScene: SKScene {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }
         let location = touch.location(in: self)
-        let walkAnimation =
-        [
-            SKTexture(imageNamed: "sprite_0"),
-            SKTexture(imageNamed: "sprite_1"),
-            SKTexture(imageNamed: "sprite_2"),
-            SKTexture(imageNamed: "sprite_3"),
-            SKTexture(imageNamed: "sprite_4"),
-            SKTexture(imageNamed: "sprite_5"),
-            SKTexture(imageNamed: "sprite_6")
-        ]
+        let kyo = KYOPriteNode(imageNamed: "kyo_run_01")
+        kyo.makeAction(type: .jump)
+        kyo.position = location
+        kyo.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
         
-        let box = SKSpriteNode(texture: walkAnimation[0])
-        box.run(SKAction.animate(with: walkAnimation, timePerFrame: 0.05))
-        box.position = location
-        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
-        
-        addChild(box)
+        addChild(kyo)
     }
 }
 
 // A sample SwiftUI creating a GameScene and sizing it
-// at 300x400 points
 struct MainGameScene: View {
     var scene: SKScene {
         let scene = GameScene()
