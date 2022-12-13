ac2523d41d04defc6ece7731bc8b6844aab11ff8
aespo22 <113534388+aespo22@users.noreply.github.com>
2022-12-14 00:55:48 +0100

    Playview background update

--- Space Ninja/PlayView.swift
+++ Space Ninja/PlayView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct PlayView: View {
     var body: some View {
         
+        
+        ZStack{
         VStack {
             HStack{
                 Image("life_icon")
@@ -17,95 +19,65 @@ struct PlayView: View {
                     .aspectRatio(contentMode: .fill)
                     .padding(.bottom, -5.0)
                     .frame(width: 50, height: 50);
-                
                 Text("7")
-
-                    
-
-                Image("wool_ball_icon")
+Image("wool_ball_icon")
                     .resizable()
                         .aspectRatio(contentMode: .fill)
                         .padding(.trailing, 1.0)
                         .frame(width: 40, height: 40)
-                
-                
                 Text("00")
-                
                 Spacer()
-
-                
-                VStack {Text("TIME")
+VStack {Text("TIME")
                         .padding(.leading, -10.0)
                     Text ("00")
-                        .padding(.leading, -10.0)
-                    
-                    
-                    
-                }
-                
-                .padding()
-
-                
-                ;
+                        .padding(.leading, -10.0)}
+                .padding();
                 Spacer()
-                
-                
-               
-                
                 Spacer()
             }
-          
-            
-            
-            Spacer()
-            
+          Spacer()
             HStack {
                 Image ("joypad")
                     .resizable()
-                       .frame(width: 100, height: 100)
-                
-                       .opacity(0.3)
-                
-                       .padding()
-
-                
+                    .frame(width: 100, height: 100)
+                    .opacity(0.3)
+                    .padding()
                 ;
                 Spacer()
-                
-                
                 Image ("button0")
-                
-                    .resizable()
-                       .frame(width: 60, height: 60)
-                
-                       .opacity(0.3)
-                
-                ;
+                .resizable() .frame(width: 60, height: 60)
+                .opacity(0.3);
                 VStack {Image ("button2")
                         .resizable()
-                           .frame(width: 40, height: 40)
-                    
-                           .opacity(0.3)
-                    
-                    
-                    ;
-                    
-                    
-                    
+                        .frame(width: 40, height: 40)
+                        .opacity(0.3);
                     Image ("button1")
                         .resizable()
-                           .frame(width: 60, height: 60)
-                    
-                           .opacity(0.3)
-                    
-                    ;}
-                
-                
-            }
+                        .frame(width: 60, height: 60)
+                        .opacity(0.3);}}
         }
+            
+            
+            
+                .background(Image("finalbackground")
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all))
+
+       
+
+            
+            
+        }
+        
+      
+        
         
     }
 }
+
+
+
+
 
 struct PlayView_Previews: PreviewProvider {
     static var previews: some View {
