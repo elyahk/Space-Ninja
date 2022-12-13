67fa0b0372218c1ea8e52dabca51de084ce33f51
aespo22 <113534388+aespo22@users.noreply.github.com>
2022-12-14 00:37:20 +0100

    Play View creation , eldor please help us 😂

--- Space Ninja/PlayView.swift
+++ Space Ninja/PlayView.swift
@@ -13,9 +13,40 @@ struct PlayView: View {
         VStack {
             HStack{
                 Image("life_icon")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .padding(.bottom, -5.0)
+                    .frame(width: 50, height: 50);
+                
+                Text("7")
+
                     
 
-                Image("wool_ball_icon");
+                Image("wool_ball_icon")
+                    .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .padding(.trailing, 1.0)
+                        .frame(width: 40, height: 40)
+                
+                
+                Text("00")
+                
+                Spacer()
+
+                
+                VStack {Text("TIME")
+                        .padding(.leading, -10.0)
+                    Text ("00")
+                        .padding(.leading, -10.0)
+                    
+                    
+                    
+                }
+                
+                .padding()
+
+                
+                ;
                 Spacer()
                 
                 
@@ -29,11 +60,42 @@ struct PlayView: View {
             Spacer()
             
             HStack {
-                Image ("joypad");
+                Image ("joypad")
+                    .resizable()
+                       .frame(width: 100, height: 100)
+                
+                       .opacity(0.5)
+                
+                ;
                 Spacer()
-                Image ("button0");
-                Image ("button1");
-                Image ("button2");
+                
+                
+                Image ("button0")
+                
+                    .resizable()
+                       .frame(width: 60, height: 60)
+                
+                       .opacity(0.5)
+                
+                ;
+                VStack {Image ("button2")
+                        .resizable()
+                           .frame(width: 40, height: 40)
+                    
+                           .opacity(0.5)
+                    
+                    
+                    ;
+                    
+                    
+                    
+                    Image ("button1")
+                        .resizable()
+                           .frame(width: 60, height: 60)
+                    
+                           .opacity(0.5)
+                    
+                    ;}
                 
             }
         }
