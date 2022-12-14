1e78f5b117adbb31fc7f3480a8caf43beaf1ce2a
Eldorbek Nusratov <eldorbek.nusratov.98@gmail.com>
2022-12-14 17:06:52 +0100

    Make music struct

---
+++ Space Ninja/Helpers/Music.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  Space Ninja
+//
+//  Created by eldorbek nusratov on 12/14/22.
+//
+
+import Foundation
+
+struct Music {
+    static let background = "background_music"
+    static let pressButton = "press_play_button"
+    static let shadowAppear = "shadow_appear"
+}
+
