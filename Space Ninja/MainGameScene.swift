9f9554153e00572f21d9dfff7209b9ec38b1e91b
Eldorbek Nusratov <eldorbek.nusratov.98@gmail.com>
2022-12-14 12:56:37 +0100

    Moving

--- Space Ninja/MainGameScene.swift
+++ Space Ninja/MainGameScene.swift
@@ -24,6 +24,7 @@ class GameScene: SKScene {
         stage.size = CGSize(width: width, height: 100.0)
         stage.physicsBody?.categoryBitMask = stageCategory
         stage.physicsBody = SKPhysicsBody(rectangleOf: stage.size)
+        stage.physicsBody?.collisionBitMask = kyoCategory
         stage.anchorPoint = CGPoint(x: 0, y: 0)
         stage.position = CGPoint(x: 0, y: 0)
         stage.physicsBody?.affectedByGravity = false
@@ -34,7 +35,7 @@ class GameScene: SKScene {
     }()
 
     lazy var background: SKSpriteNode = {
-        var background = SKSpriteNode(imageNamed: "main_background")
+        var background = SKSpriteNode(imageNamed: "finalbackground_2")
         background.size = CGSize(width: width, height: height - stage.size.height)
         background.physicsBody = SKPhysicsBody(rectangleOf: background.size)
         background.anchorPoint = CGPoint(x: 0, y: 0)
@@ -120,12 +121,72 @@ class GameScene: SKScene {
     }
 }
 
+class GameScene2: SKScene {
+    let stageCategory: UInt32 = 0x1 << 1
+    let enemyCategory: UInt32 = 0x1 << 2
+    let kyoCategory: UInt32 = 0x1 << 3
+    let shadowCategory: UInt32 = 0x1 << 4
+
+    lazy var stage: SKSpriteNode = {
+        let stage = SKSpriteNode(color: .systemGray, size: .init(width: width, height: 80))
+        stage.anchorPoint = .init(x: 0.5, y: 0.5)
+        stage.position = .init(x: width / 2, y: stage.size.height/2)
+        stage.physicsBody = SKPhysicsBody(rectangleOf: stage.size)
+        stage.physicsBody?.affectedByGravity = false
+        stage.physicsBody?.isDynamic = false
+        stage.physicsBody?.categoryBitMask = stageCategory
+        stage.zPosition = -1
+        stage.physicsBody?.friction = 0.8
+
+        return stage
+    }()
+
+    lazy var kyo: KYO = {
+        let kyo = KYO(texture: SKTexture(imageNamed: "kyo_attack_1"))
+        kyo.size = CGSize(width: 50, height: 50)
+        kyo.physicsBody = SKPhysicsBody(rectangleOf: kyo.size)
+        kyo.physicsBody?.allowsRotation = false
+        kyo.physicsBody?.isDynamic = true
+        kyo.physicsBody?.affectedByGravity = true
+        kyo.anchorPoint = .init(x: 0.5, y: 0.5)
+        kyo.position = CGPoint(x: 10, y: 200)
+        kyo.physicsBody?.categoryBitMask = kyoCategory
+        kyo.physicsBody?.collisionBitMask = stageCategory
+
+        return kyo
+    }()
+
+    override func didMove(to view: SKView) {
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        physicsWorld.gravity = CGVector(dx: 0.0, dy: -3.0)
+        addChild(stage)
+        addChild(kyo)
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        move()
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        stop()
+    }
+
+    private func move() {
+        kyo.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 0))
+    }
+
+    private func stop() {
+        kyo.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+    }
+}
+
+
 // A sample SwiftUI creating a GameScene and sizing it
 struct MainGameScene: View {
     var scene: SKScene {
-        let scene = GameScene()
-        scene.size = CGSize(width: width, height: height)
+        let scene = GameScene2()
         scene.scaleMode = .resizeFill
+
         return scene
     }
 
@@ -135,6 +196,17 @@ struct MainGameScene: View {
                 .ignoresSafeArea()
             VStack {
                 Spacer()
+                HStack {
+                    Button("Left") {
+
+                    }
+                    .padding()
+                    Button("Left") {
+
+                    }
+                    .padding()
+                    Spacer()
+                }
             }
         }
     }
@@ -144,7 +216,6 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         MainGameScene()
             .previewInterfaceOrientation(.landscapeLeft)
-            .environment(\.horizontalSizeClass, .compact) // 2
     }
 }
 
