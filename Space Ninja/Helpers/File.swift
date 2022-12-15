86ac21e68ca0b39a6872d4d17f72cbbf3513626a
Eldorbek Nusratov <eldorbek.nusratov.98@gmail.com>
2022-12-15 15:20:22 +0100

    Put buttons

---
+++ Space Ninja/Helpers/File.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  Space Ninja
+//
+//  Created by eldorbek nusratov on 12/15/22.
+//
+
+import Foundation
+
+extension String {
+    static let swordImage = "button0"
+    static let jumpImage = "button1"
+    static let shurikenImage = "button2"
+    static let leftRunImage = "joypad_button_left"
+    static let rightRunImage = "joypad_button_right"
+    static let lifeIcon = "life_icon"
+    static let woolBallIcon = "wool_ball_icon"
+}
