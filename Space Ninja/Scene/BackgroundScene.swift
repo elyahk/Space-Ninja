af211b126caf32fa791b6d6258c629bac9d7ff31
Eldorbek Nusratov <eldorbek.nusratov.98@gmail.com>
2022-12-15 16:43:23 +0100

    Move all configuration to their own methods

--- Space Ninja/Scene/BackgroundScene.swift
+++ Space Ninja/Scene/BackgroundScene.swift
@@ -6,3 +6,194 @@
 //
 
 import Foundation
+import SpriteKit
+
+class BackgroundScene {
+    var lifeCount: Int = 3 {
+        didSet {
+            lifeText.text = "\(lifeCount)"
+        }
+    }
+
+    var ballCount: Int = 0 {
+        didSet {
+            ballText.text = "\(ballCount)"
+        }
+    }
+
+    lazy var stage: SKSpriteNode = {
+        let stage = SKSpriteNode(texture: SKTexture(imageNamed: "ground_01"), size: .init(width: width, height: 80))
+        stage.anchorPoint = .init(x: 0.5, y: 0.5)
+        stage.position = .init(x: width / 2, y: stage.size.height/2)
+        stage.physicsBody = SKPhysicsBody(rectangleOf: stage.size)
+        stage.physicsBody?.affectedByGravity = false
+        stage.physicsBody?.isDynamic = false
+        stage.physicsBody?.categoryBitMask = PhysicsCategory.ground
+        stage.zPosition = -1
+        stage.physicsBody?.friction = 0
+
+        return stage
+    }()
+
+    lazy var background: SKSpriteNode = {
+        let stage = SKSpriteNode(texture: SKTexture(imageNamed: "finalbackground"), size: .init(width: width, height: height))
+        stage.anchorPoint = .init(x: 0.5, y: 0.5)
+        stage.position = .init(x: width / 2, y: height / 2 )
+        stage.zPosition = -10
+
+        return stage
+    }()
+
+    lazy var lifeIcon: SKSpriteNode = {
+        let stage = SKSpriteNode(texture: SKTexture(imageNamed: .lifeIcon))
+        stage.size = .init(width: 30, height: 30)
+        stage.anchorPoint = .init(x: 0.5, y: 0.5)
+        stage.position = .init(x: 35, y: height - 30)
+        stage.zPosition = -2
+
+        return stage
+    }()
+
+    lazy var lifeText: SKLabelNode = {
+        var view = SKLabelNode()
+        view = SKLabelNode(fontNamed: "AvenirNext-Bold")
+        view.text = "\(lifeCount)"
+        view.horizontalAlignmentMode = .left
+        view.fontSize = 35
+        view.position = .init(x: lifeIcon.position.x + lifeIcon.size.width / 2 + 10, y: lifeIcon.position.y - 10)
+
+        return view
+    }()
+
+    lazy var ballIcon: SKSpriteNode = {
+        let stage = SKSpriteNode(texture: SKTexture(imageNamed: .woolBallIcon))
+        stage.size = .init(width: 30, height: 30)
+        stage.anchorPoint = .init(x: 0.5, y: 0.5)
+        stage.position = .init(x: 125, y: height - 30)
+        stage.zPosition = -2
+
+        return stage
+    }()
+
+    lazy var ballText: SKLabelNode = {
+        var view = SKLabelNode()
+        view = SKLabelNode(fontNamed: "AvenirNext-Bold")
+        view.text = "\(ballCount)"
+        view.horizontalAlignmentMode = .left
+        view.fontSize = 35
+        view.position = .init(x: ballIcon.position.x + ballIcon.size.width / 2 + 10, y: ballIcon.position.y - 10)
+
+        return view
+    }()
+
+    lazy var planet1: SKSpriteNode = {
+        let stage = SKSpriteNode(texture: SKTexture(imageNamed: "planet_02"))  // saturn
+        stage.size = .init(width: 50, height: 50)
+        stage.anchorPoint = .init(x: 0.5, y: 0.5)
+        stage.position = .init(x: 240 , y: height - 190 )
+        stage.zPosition = -2
+
+        return stage
+    }()
+
+    lazy var planet2: SKSpriteNode = {
+        let stage = SKSpriteNode(texture: SKTexture(imageNamed: "sun_1")) //sun
+        stage.size = .init(width: 100, height: 100)
+        stage.anchorPoint = .init(x: 0.5, y: 0.5)
+        stage.position = .init(x: width / 2, y: 250 )
+        stage.zPosition = -2
+
+        return stage
+    }()
+
+    lazy var planet3: SKSpriteNode = {
+        let stage = SKSpriteNode(texture: SKTexture(imageNamed: "planet_01"))
+        stage.size = .init(width: 30, height: 30)
+        stage.anchorPoint = .init(x: 0.5, y: 0.5)
+        stage.position = .init(x: 800, y: height - 100)
+        stage.zPosition = -2
+
+        return stage
+    }()
+
+
+
+
+
+    lazy var planet4: SKSpriteNode = {
+        let stage = SKSpriteNode(texture: SKTexture(imageNamed: "woll_ball_01")) //wool
+        stage.size = .init(width: 60, height: 60)
+        stage.anchorPoint = .init(x: 0.5, y: 0.5)
+        stage.position = .init(x: 700 , y: height-120 )
+        stage.zPosition = -2
+
+        return stage
+    }()
+
+
+
+    lazy var planet5: SKSpriteNode = {
+        let stage = SKSpriteNode(texture: SKTexture(imageNamed: "woll_ball_01")) //wool
+        stage.size = .init(width: 60, height: 60)
+        stage.anchorPoint = .init(x: 0.5, y: 0.5)
+        stage.position = .init(x: 700 , y: height-120 )
+        stage.zPosition = -2
+
+        return stage
+    }()
+
+
+    lazy var planet6: SKSpriteNode = {
+        let stage = SKSpriteNode(texture: SKTexture(imageNamed: "woll_ball_01")) //wool
+        stage.size = .init(width: 60, height: 60)
+        stage.anchorPoint = .init(x: 0.5, y: 0.5)
+        stage.position = .init(x: 700 , y: height-120 )
+        stage.zPosition = -2
+
+        return stage
+    }()
+
+
+    lazy var planet7: SKSpriteNode = {
+        let stage = SKSpriteNode(texture: SKTexture(imageNamed: "sun_1")) //sun piccolo
+        stage.size = .init(width: 50, height: 50)
+        stage.anchorPoint = .init(x: 0.5, y: 0.5)
+        stage.position = .init(x: 500, y: height  - 150 )
+        stage.zPosition = -2
+
+        return stage
+
+    }()
+
+    lazy var planet8: SKSpriteNode = {
+        let stage = SKSpriteNode(texture: SKTexture(imageNamed: "planet_01"))
+        stage.size = .init(width: 40, height: 40)
+        stage.anchorPoint = .init(x: 0.5, y: 0.5)
+        stage.position = .init(x: 820, y: height - 80)
+        stage.zPosition = -2
+
+        return stage
+    }()
+
+    lazy var planet9: SKSpriteNode = {
+        let stage = SKSpriteNode(texture: SKTexture(imageNamed: "planet_01"))
+        stage.size = .init(width: 30, height: 30)
+        stage.anchorPoint = .init(x: 0.5, y: 0.5)
+        stage.position = .init(x: 850, y: height - 120)
+        stage.zPosition = -2
+
+        return stage
+    }()
+
+    lazy var planets: [SKSpriteNode] = [
+        planet1,
+        planet2,
+        planet3,
+        planet4,
+        planet5,
+        planet6,
+        planet7,
+        planet8,
+        planet9
+    ]
+}
--- Space Ninja/Scene/GameScene.swift
+++ Space Ninja/Scene/GameScene.swift
@@ -15,6 +15,7 @@ class GameScene: SKScene {
     let shadowCategory: UInt32 = 0x1 << 4
     var shurikens: [SKSpriteNode] = []
     var console: Console = .init()
+    var bacgroundScene: BackgroundScene = .init()
 
     var lifeCount: Int = 3 {
         didSet {
@@ -28,175 +29,14 @@ class GameScene: SKScene {
         }
     }
 
-    lazy var stage: SKSpriteNode = {
-        let stage = SKSpriteNode(texture: SKTexture(imageNamed: "ground_01"), size: .init(width: width, height: 80))
-        stage.anchorPoint = .init(x: 0.5, y: 0.5)
-        stage.position = .init(x: width / 2, y: stage.size.height/2)
-        stage.physicsBody = SKPhysicsBody(rectangleOf: stage.size)
-        stage.physicsBody?.affectedByGravity = false
-        stage.physicsBody?.isDynamic = false
-        stage.physicsBody?.categoryBitMask = PhysicsCategory.ground
-        stage.zPosition = -1
-        stage.physicsBody?.friction = 0
-
-        return stage
-    }()
-
-    lazy var background: SKSpriteNode = {
-        let stage = SKSpriteNode(texture: SKTexture(imageNamed: "finalbackground"), size: .init(width: width, height: height))
-        stage.anchorPoint = .init(x: 0.5, y: 0.5)
-        stage.position = .init(x: width / 2, y: height / 2 )
-        stage.zPosition = -10
-
-        return stage
-    }()
-
-    lazy var lifeIcon: SKSpriteNode = {
-        let stage = SKSpriteNode(texture: SKTexture(imageNamed: .lifeIcon))
-        stage.size = .init(width: 30, height: 30)
-        stage.anchorPoint = .init(x: 0.5, y: 0.5)
-        stage.position = .init(x: 35, y: height - 30)
-        stage.zPosition = -2
-
-        return stage
-    }()
-
-    lazy var lifeText: SKLabelNode = {
-        var view = SKLabelNode()
-        view = SKLabelNode(fontNamed: "AvenirNext-Bold")
-        view.text = "\(lifeCount)"
-        view.horizontalAlignmentMode = .left
-        view.fontSize = 35
-        view.position = .init(x: lifeIcon.position.x + lifeIcon.size.width / 2 + 10, y: lifeIcon.position.y - 10)
-
-        return view
-    }()
-
-    lazy var ballIcon: SKSpriteNode = {
-        let stage = SKSpriteNode(texture: SKTexture(imageNamed: .woolBallIcon))
-        stage.size = .init(width: 30, height: 30)
-        stage.anchorPoint = .init(x: 0.5, y: 0.5)
-        stage.position = .init(x: 125, y: height - 30)
-        stage.zPosition = -2
-
-        return stage
-    }()
-
-    lazy var ballText: SKLabelNode = {
-        var view = SKLabelNode()
-        view = SKLabelNode(fontNamed: "AvenirNext-Bold")
-        view.text = "\(ballCount)"
-        view.horizontalAlignmentMode = .left
-        view.fontSize = 35
-        view.position = .init(x: ballIcon.position.x + ballIcon.size.width / 2 + 10, y: ballIcon.position.y - 10)
-
-        return view
-    }()
-
-    lazy var planet1: SKSpriteNode = {
-        let stage = SKSpriteNode(texture: SKTexture(imageNamed: "planet_02"))  // saturn
-        stage.size = .init(width: 50, height: 50)
-        stage.anchorPoint = .init(x: 0.5, y: 0.5)
-        stage.position = .init(x: 240 , y: height - 190 )
-        stage.zPosition = -2
-
-        return stage
-    }()
-
-    lazy var planet2: SKSpriteNode = {
-        let stage = SKSpriteNode(texture: SKTexture(imageNamed: "sun_1")) //sun
-        stage.size = .init(width: 100, height: 100)
-        stage.anchorPoint = .init(x: 0.5, y: 0.5)
-        stage.position = .init(x: width / 2, y: 250 )
-        stage.zPosition = -2
-
-        return stage
-    }()
-
-    lazy var planet3: SKSpriteNode = {
-        let stage = SKSpriteNode(texture: SKTexture(imageNamed: "planet_01"))
-        stage.size = .init(width: 30, height: 30)
-        stage.anchorPoint = .init(x: 0.5, y: 0.5)
-        stage.position = .init(x: 800, y: height - 100)
-        stage.zPosition = -2
-
-        return stage
-    }()
-    
-    
-    
-    
-
-    lazy var planet4: SKSpriteNode = {
-        let stage = SKSpriteNode(texture: SKTexture(imageNamed: "woll_ball_01")) //wool
-        stage.size = .init(width: 60, height: 60)
-        stage.anchorPoint = .init(x: 0.5, y: 0.5)
-        stage.position = .init(x: 700 , y: height-120 )
-        stage.zPosition = -2
-
-        return stage
-    }()
-    
-    
-    
-    lazy var planet5: SKSpriteNode = {
-        let stage = SKSpriteNode(texture: SKTexture(imageNamed: "woll_ball_01")) //wool
-        stage.size = .init(width: 60, height: 60)
-        stage.anchorPoint = .init(x: 0.5, y: 0.5)
-        stage.position = .init(x: 700 , y: height-120 )
-        stage.zPosition = -2
-
-        return stage
-    }()
-    
-    
-    lazy var planet6: SKSpriteNode = {
-        let stage = SKSpriteNode(texture: SKTexture(imageNamed: "woll_ball_01")) //wool
-        stage.size = .init(width: 60, height: 60)
-        stage.anchorPoint = .init(x: 0.5, y: 0.5)
-        stage.position = .init(x: 700 , y: height-120 )
-        stage.zPosition = -2
-
-        return stage
-    }()
-    
-    
-    lazy var planet7: SKSpriteNode = {
-        let stage = SKSpriteNode(texture: SKTexture(imageNamed: "sun_1")) //sun piccolo
-        stage.size = .init(width: 50, height: 50)
-        stage.anchorPoint = .init(x: 0.5, y: 0.5)
-        stage.position = .init(x: 500, y: height  - 150 )
-        stage.zPosition = -2
-
-        return stage
-    
-    }()
-    
-    lazy var planet8: SKSpriteNode = {
-        let stage = SKSpriteNode(texture: SKTexture(imageNamed: "planet_01"))
-        stage.size = .init(width: 40, height: 40)
-        stage.anchorPoint = .init(x: 0.5, y: 0.5)
-        stage.position = .init(x: 820, y: height - 80)
-        stage.zPosition = -2
-
-        return stage
-    }()
-    
-    lazy var planet9: SKSpriteNode = {
-        let stage = SKSpriteNode(texture: SKTexture(imageNamed: "planet_01"))
-        stage.size = .init(width: 30, height: 30)
-        stage.anchorPoint = .init(x: 0.5, y: 0.5)
-        stage.position = .init(x: 850, y: height - 120)
-        stage.zPosition = -2
-
-        return stage
-    }()
-    
-    
-    
-    
-    
-
+    lazy var stage: SKSpriteNode = bacgroundScene.stage
+    lazy var background: SKSpriteNode = bacgroundScene.background
+    lazy var lifeIcon: SKSpriteNode = bacgroundScene.lifeIcon
+    lazy var lifeText: SKLabelNode = bacgroundScene.lifeText
+    lazy var ballIcon: SKSpriteNode = bacgroundScene.ballIcon
+    lazy var ballText: SKLabelNode = bacgroundScene.ballText
+    lazy var planets: [SKSpriteNode] = bacgroundScene.planets
+    lazy var planet1: SKSpriteNode = bacgroundScene.planet1
     lazy var shadow: Shadow = {
         let shadow = Shadow.makeShadow()
 
@@ -212,31 +52,39 @@ class GameScene: SKScene {
     }
 
     private func configure() {
-        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
-        physicsWorld.gravity = CGVector(dx: 0.0, dy: -3.0)
-        physicsWorld.contactDelegate = self
-        addChild(background)
-        addChild(lifeIcon)
-        addChild(lifeText)
-        addChild(ballIcon)
-        addChild(ballText)
-        addChild(planet1)
-        addChild(planet2)
-        addChild(planet3)
-        addChild(planet4)
-        addChild(planet5)
-        addChild(planet6)
-        addChild(planet7)
-        addChild(planet8)
-        addChild(planet9)
-        addChild(stage)
+        configurePhysics()
+        configureBackgroundScene()
+        configureCharacters()
+        configureConsole()
+    }
+
+    private func configureCharacters() {
         addChild(kyo)
         addChild(shadow)
+    }
+
+    private func configureConsole() {
         addChild(console.leftButton)
         addChild(console.rightButton)
         addChild(console.jumpButton)
         addChild(console.shootButton)
         addChild(console.attackButton)
+    }
+
+    private func configurePhysics() {
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        physicsWorld.gravity = CGVector(dx: 0.0, dy: -3.0)
+        physicsWorld.contactDelegate = self
+    }
+
+    private func configureBackgroundScene() {
+        addChild(stage)
+        addChild(background)
+        addChild(lifeIcon)
+        addChild(lifeText)
+        addChild(ballIcon)
+        addChild(ballText)
+        planets.forEach { addChild($0) }
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
